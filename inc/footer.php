e143eec6c891a2d0495cb5930fc3dae3f7f0a681
Travis <avey.travis@gmail.com>
2014-08-21 12:31:55 -0500

    Updated to include header.php and footer.php

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<p>&copy;<?php echo date('Y'); ?>  | Web Development &amp; Design by CypherBytes</p>
+</footer>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="../js/build/production.min.js"></script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="/Boilerplate/css/style.css">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta charset="utf-8" />
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js"></script>
+	<!-- [if lt IE 10]> <script src="http://html5shim.googlecode.com/svn/trunk/html5-els.js"></script> <![endif] -->		
+
+	<title></title>
+</head>
+<body>
